51203aeb69ffb78304beb8d35d0bbc310d5b9742
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2025-10-01 05:39:10 -0300

    feat/dto: Adicionado DTOs de erros

---
+++ JavaSpringProfessional/desafio03/apirest/src/main/java/com/devsuperior/apirest/dto/FieldMessage.java
@@ -0,0 +1,31 @@
+package com.devsuperior.apirest.dto;
+
+public class FieldMessage {
+
+    private String name;
+    private String message;
+
+    public FieldMessage() {
+    }
+
+    public FieldMessage(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

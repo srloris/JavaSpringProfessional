900cdaf63d57e7d26ff63c6b020f18d02d9dc426
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2025-10-01 05:34:23 -0300

    feat/config: Configuração da aplicação

---
+++ JavaSpringProfessional/desafio03/apirest/src/test/java/com/devsuperior/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

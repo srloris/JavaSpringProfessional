31919318ee0e68aba9de9927438bfcdfe65d45c8
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2025-10-08 08:07:17 -0300

    fix/service: Correção dos metodos de delete e update / Adição do Lombok

--- JavaSpringProfessional/desafio03/apirest/src/main/java/com/devsuperior/apirest/services/ClientService.java
+++ JavaSpringProfessional/desafio03/apirest/src/main/java/com/devsuperior/apirest/services/ClientService.java
@@ -4,26 +4,22 @@ import com.devsuperior.apirest.dto.ClientDTO;
 import com.devsuperior.apirest.entities.Client;
 import com.devsuperior.apirest.mapper.ClientMapper;
 import com.devsuperior.apirest.repositories.ClientRepository;
-import com.devsuperior.apirest.services.Expections.DatabaseException;
-import com.devsuperior.apirest.services.Expections.ResourceNotFoundException;
+import com.devsuperior.apirest.services.exceptions.DatabaseException;
+import com.devsuperior.apirest.services.exceptions.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.SQLException;
-
 @Service
+@RequiredArgsConstructor
 public class ClientService {
 
     private final ClientRepository repository;
     private final ClientMapper mapper;
-
-    public ClientService(ClientRepository repository, ClientMapper mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
-    }
 
     @Transactional(readOnly = true)
     public Page<ClientDTO> findAll(Pageable pageable) {
@@ -46,17 +42,23 @@ public class ClientService {
 
     @Transactional
     public ClientDTO update(Long id, ClientDTO clientDTO) {
-        if(!repository.existsById(id)) throw new ResourceNotFoundException("Resource not found");
-        return insert(clientDTO);
+        Client client = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
+
+        mapper.updateEntityFromDTO(clientDTO, client);
+        Client updated = repository.save(client);
+        return mapper.toDTO(updated);
     }
 
     @Transactional
     public void delete(Long id) {
-        if(!repository.existsById(id)) throw new ResourceNotFoundException("Resource not found");
         try {
             repository.deleteById(id);
+            repository.flush(); // força a execução imediata
         } catch(DataIntegrityViolationException e) {
             throw new DatabaseException("Referential Integrity Failure");
+        } catch(EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Resource not found");
         }
     }
 

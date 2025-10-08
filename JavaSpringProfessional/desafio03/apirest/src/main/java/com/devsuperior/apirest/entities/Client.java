91d68126b94cd6cb34b6eb70e1f718a2988efb70
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2025-10-08 08:03:23 -0300

    feat/entities: Adicionado anotações do Lombok

--- JavaSpringProfessional/desafio03/apirest/src/main/java/com/devsuperior/apirest/entities/Client.java
+++ JavaSpringProfessional/desafio03/apirest/src/main/java/com/devsuperior/apirest/entities/Client.java
@@ -1,12 +1,19 @@
 package com.devsuperior.apirest.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
 @Table(name = "tb_client")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client {
 
     @Id
@@ -20,76 +27,4 @@ public class Client {
     private LocalDate birthDate;
     private Integer children;
 
-    public Client() {
-
-    }
-
-    public Client(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
-        this.id = id;
-        this.name = name;
-        this.cpf = cpf;
-        this.income = income;
-        this.birthDate = birthDate;
-        this.children = children;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Double getIncome() {
-        return income;
-    }
-
-    public void setIncome(Double income) {
-        this.income = income;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Integer getChildren() {
-        return children;
-    }
-
-    public void setChildren(Integer children) {
-        this.children = children;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return Objects.equals(cpf, client.cpf);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(cpf);
-    }
 }
